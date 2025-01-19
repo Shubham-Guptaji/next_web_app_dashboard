3d3a43e657d439f321992030aed217188870fe6e
Shubham Gupta <shubhamguptaji81@gmail.com>
2025-01-19 15:01:26 +0530

    Integrated Vercel Postgres

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,36 @@
-// import { db } from "@vercel/postgres";
+import { db } from "@vercel/postgres";
 
-// const client = await db.connect();
+const client = await db.connect();
 
-// async function listInvoices() {
-// 	const data = await client.sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+// import { neon } from "@neondatabase/serverless";
 
-// 	return data.rows;
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error('DATABASE_URL is not defined');
 // }
+// const sql = neon(process.env.DATABASE_URL);
+
+// const client = {sql};
+
+async function listInvoices() {
+	const data = await client.sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 666;
+  `;
+
+	return data.rows;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
